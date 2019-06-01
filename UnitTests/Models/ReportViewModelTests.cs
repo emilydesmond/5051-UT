1d8980034805bd5dc8fa12c9ddbfb34d4a5c3b29
emilydesmond <emilyjadesmond@gmail.com>
2019-05-31 20:20:10 -0700

    Added ReportViewModel LogViewModel Get Test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -47,5 +47,18 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(5, result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.LogViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
