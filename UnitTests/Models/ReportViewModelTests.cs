124182f2e9f0fd1e443bc8a5c704c11fccf1829a
emilydesmond <emilyjadesmond@gmail.com>
2019-05-31 19:04:26 -0700

    Added ReportViewModelTests.cs to the project

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+using System.Collections.Generic;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void LogViewModel_Instantiate_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new LogViewModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new LogViewModel();
+
+            // Act
+            var result = myTest.LogList;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void LogViewModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new LogViewModel();
+            var myList = new List<LogModel>();
+            myList.Add(new LogModel { PhoneID = "Phone" });
+
+            // Act
+            myTest.LogList = myList;
+            var result = myTest.LogList;
+
+            // Assert
+            Assert.AreEqual("Phone", result[0].PhoneID);
+        }
+    }
+}
